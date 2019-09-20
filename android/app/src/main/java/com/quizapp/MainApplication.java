a4b1b43c1508cd620d9f88805ee5c7127b6ed33f
garganjohn <garganjohn@gmail.com>
2019-09-20 16:19:35 -0400

    it worked then it didn't, then it did

--- android/app/src/main/java/com/quizapp/MainApplication.java
+++ android/app/src/main/java/com/quizapp/MainApplication.java
@@ -1,6 +1,6 @@
 package com.quizapp;
 
-import android.app.Application;
+import androidx.multidex.MultiDexApplication;
 import android.util.Log;
 
 import com.facebook.react.PackageList;
@@ -13,7 +13,7 @@ import com.facebook.soloader.SoLoader;
 
 import java.util.List;
 
-public class MainApplication extends Application implements ReactApplication {
+public class MainApplication extends MultiDexApplication implements ReactApplication {
 
   private final ReactNativeHost mReactNativeHost = new ReactNativeHost(this) {
     @Override
